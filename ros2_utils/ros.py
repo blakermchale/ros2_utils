41e531cb4a89bcbef33fae852fd52d1b6223ebc9
Blake McHale <mchale.blake@gmail.com>
2022-01-08 12:31:29 -0500

    Add function for checking ROS msg for nans

--- ros2_utils/ros.py
+++ ros2_utils/ros.py
@@ -1,11 +1,13 @@
 from typing import Union
 from .math import convert_axes, AxesFrame
-from geometry_msgs.msg import Pose, Transform, Twist
+from geometry_msgs.msg import Pose, Transform, Twist, Point, Vector3
 from nav_msgs.msg import Odometry
 from .structs import NpVector4, NpPose, NpVector3, NpTwist
+import numpy as np
 
 
 def convert_axes_from_msg(msg: Union[Pose, Transform, Twist, Odometry], in_axes: AxesFrame, out_axes: AxesFrame):
+    """Converts ROS message coordinate frame."""
     if isinstance(msg, Pose):
         q = NpVector4.ros(msg.orientation)
         x, y, z, roll, pitch, yaw = convert_axes(msg.position.x, msg.position.y, msg.position.z, q.roll, q.pitch, q.yaw, in_axes, out_axes)
@@ -27,5 +29,13 @@ def convert_axes_from_msg(msg: Union[Pose, Transform, Twist, Odometry], in_axes:
         x, y, z, roll, pitch, yaw = convert_axes(twist_msg.linear.x, twist_msg.linear.y, twist_msg.linear.z, twist_msg.angular.x, twist_msg.angular.y, twist_msg.angular.z, in_axes, out_axes)
         out_msg.twist.twist = NpTwist(NpVector3.xyz(x, y, z), NpVector3.xyz(roll, pitch, yaw)).get_msg()
         return out_msg
+    else:
+        raise ValueError(f"ROS message type {type(msg)} is not supported")
+
+
+def msg_contains_nan(msg: Union[Point, Vector3]):
+    """Checks if ROS message contains any nans."""
+    if isinstance(msg, (Point, Vector3)):
+        return np.isnan(msg.x) or np.isnan(msg.y) or np.isnan(msg.z)
     else:
         raise ValueError(f"ROS message type {type(msg)} is not supported")
